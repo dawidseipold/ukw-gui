0ea8d43c0ff23571750ee5b5dd42c1cec19eb5bf
dawidseipold <seipold.dawid@gmail.com>
2025-05-31 12:06:35 +0200

    feat: add a basic tab menu

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-import tkinter
+import tkinter as tk
 from tkinter import ttk
 
 import sv_ttk
 
-root = tkinter.Tk()
+from components.topmenu import TopMenu, TabContent1, TabContent2
 
-button = ttk.Button(root, text="Click me!")
-button.pack()
+root = tk.Tk()
+
+root.title("Cwiczenie 5")
+root.geometry("600x400")
+
+top_menu = TopMenu(root)
+top_menu.pack(fill=tk.BOTH, expand=True)
+
+top_menu.add_tab("Tab 1", TabContent1)
+top_menu.add_tab("Tab 2", TabContent2)
 
 sv_ttk.set_theme("light")
 
